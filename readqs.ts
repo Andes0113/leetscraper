b70a31a13376188bdc9c5f0c5c9fbafa6ce997ea
Andes0113 <54319516+Andes0113@users.noreply.github.com>
2024-08-26 19:39:34 -0500

    Created Batch Embedding Request

--- readqs.ts
+++ readqs.ts
@@ -1,3 +1,5 @@
+import type { EmbeddingCreateParams } from 'openai/resources/embeddings';
+
 const data = await Bun.file('questions.json').text();
 
 type Question = {
@@ -16,6 +18,19 @@ type Question = {
 
 const { questions } = JSON.parse(data) as { questions: Question[] };
 
-console.log(questions[0].starterCode.python);
+type BatchEmbeddingRequest = EmbeddingCreateParams & { custom_id: string };
+
+const embeddingRequestFileContent: string = questions
+  .map(
+    (question): BatchEmbeddingRequest => ({
+      custom_id: question.id,
+      input: question.solutions.python,
+      model: 'text-embedding-3-large',
+    })
+  )
+  .map((request) => JSON.stringify(request))
+  .join('\n');
+
+await Bun.write('embeddingBatchRequest.jsonl', embeddingRequestFileContent);
 
 export {};
